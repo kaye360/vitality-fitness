cafe928a6bc4586e59ab21dfe5dc15c80b0d5fc0
joshkaye88 <jkaye@shaw.ca>
2023-03-10 22:08:25 -0800

    start page header

--- src/content/facility.md
+++ src/content/facility.md
@@ -1,5 +1,6 @@
 ---
 title: Explore the Facility
 ---
+
 Vitality Fitness is located in the heart of Willoughby, a community in Langley, BC. The gym features everything you need for your personal fitness journey.
 (Images, slideshow)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					100: 'hsl(var(--color-primary-100) / <alpha-value>)',
+					200: 'hsl(var(--color-primary-200) / <alpha-value>)',
+					300: 'hsl(var(--color-primary-300) / <alpha-value>)',
+					400: 'hsl(var(--color-primary-400) / <alpha-value>)',
+					500: 'hsl(var(--color-primary-500) / <alpha-value>)',
+					600: 'hsl(var(--color-primary-600) / <alpha-value>)',
+					700: 'hsl(var(--color-primary-700) / <alpha-value>)',
+					800: 'hsl(var(--color-primary-800) / <alpha-value>)',
+					900: 'hsl(var(--color-primary-900) / <alpha-value>)',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
